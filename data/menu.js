ac33315b8127fecea9b249cbdc3f1ef0b827953f
Jason Johnson <jas2johns@gmail.com>
2022-09-04 18:52:59 -0700

    updated menu imgs to svg's

--- data/menu.js
+++ data/menu.js
@@ -4,37 +4,37 @@ const data = {
 			id: 1,
 			description:
 				'The Go 2 classic Single Mustard Grilled Patty, "white" American Cheese, fresh shredded lettuce, tomato.',
-			image: "BURGER.jpg",
+			image: "Classic.svg",
 		},
 		{
 			id: 2,
 			description:
 				'Double, 2 Mustard Grilled Patties, "white" American Cheese, fresh shredded lettuce, tomato',
-			image: "BURGER.jpg",
+			image: "The Double.svg",
 		},
 		{
 			id: 3,
 			description:
 				"The Hawaiian burger; BBQ Grilled burger 6oz, Chopped Grilled onions, grilled pineapple slice, Monterey jack crisp, and a choice of our house teriyaki Sauce or BBQ sauce.",
-			image: "burger2.jpg",
+			image: "Hawaiin.svg",
 		},
 		{
 			id: 4,
 			description:
 				"Western, Monterey jack, crispy shallots, chipotle BBQ sauce, caramelized onions",
-			image: "BURGER.jpg",
+			image: "Western.svg",
 		},
 		{
 			id: 5,
 			description:
 				'California, Mustard Grilled Patty, Oaxacan Cheese, fresh shredded lettuce, avo slices and a side of "Pico de gallo"',
-			image: "BURGER.jpg",
+			image: "California Buger.svg",
 		},
 		{
 			id: 6,
 			description:
 				"Patty melt with caramelized onions sourdough bread or rye bread",
-			image: "BURGER.jpg",
+			image: "pattymelt.svg",
 		},
 	],
 };
