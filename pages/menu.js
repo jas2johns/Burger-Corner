a25e44249fbdda6c89f20cc8ea305d8ad1c7a217
Jason Johnson <jas2johns@gmail.com>
2022-09-22 16:39:02 -0700

    stuck implementing a simple parrallax scrollAffect

--- pages/menu.js
+++ pages/menu.js
@@ -6,6 +6,19 @@ const Menu = () => {
 	return (
 		<>
 			<div>
+							<Image
+								src="/menu.jpg"
+								alt="table with people eating around it"
+								opacity="0.65"
+								//layout="fill"
+								min-height= "500px"
+
+								/* Create the parallax scrolling effect */
+								background-attachment= "fixed"
+								background-position= "center"
+								background-repeat= "no-repeat"
+								background-size= "cover"
+							/>
 				<header>
 					<h1>Menu</h1>
 				</header>
