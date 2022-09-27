2253c8240de7ac35ad5d93d4541f244897844daf
Jason Johnson <jas2johns@gmail.com>
2022-09-27 16:21:40 -0700

    started the parallax effect

--- pages/menu.js
+++ pages/menu.js
@@ -2,6 +2,26 @@ import styles from "../styles/Menu.module.css";
 import MenuItem from "../components/MenuItem";
 import Image from "next/image";
 import data from "../data/menu";
+// new
+import menu from './menu.jpg';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+/* 
+      <Parallax pages={1} ref={ref}>
+		<ParallaxLayer
+		  offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${menu})`,
+            backgroundSize: 'cover',
+          }}>
+
+		</ParallaxLayer>
+      </Parallax>
+
+
+*/
 
 const Menu = () => {
 	return (
