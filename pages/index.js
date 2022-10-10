7d8c97ea1a66a370c84bbe608a92df993f15ee03
Jason Johnson <jas2johns@gmail.com>
2022-10-10 12:35:21 -0700

    cleaned up import statements

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
 import { ShoppingCartProvider } from "../context/ShoppingCartContext";
 
 export default function Home() {
