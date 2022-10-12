002e1861acbe8b45b6d328c425a16a566e41f78a
Jonathan Carter <carterj@belletire.com>
2022-10-12 16:09:49 -0400

    navbar stacking

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useTheme } from "../context/ThemeContext";
-import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 
 const Navbar = () => {
 	const { darkModeEnabled, setDarkModeEnabled } = useTheme();
@@ -25,57 +25,59 @@ const Navbar = () => {
 					<span className="navbar-toggler-icon" />
 				</button>
 				<div
-					className="collapse navbar-collapse mx-auto nav nav-pills nav-stacked"
+					className="collapse navbar-collapse nav nav-stacked"
 					id="navbarTogglerDemo01"
 				>
-					<Link href="/">
-						<a className="navbar-brand">Burger Corner</a>
-					</Link>
-					<ul className="navbar-nav mb-2 mb-lg-0 d-flex justify-content-evenly flex-fill">
-						<li className="nav-item">
-							<Link href="/menu">
-								<a className="nav-link">Menu</a>
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link href="/shoppingCart">
-								<a className="nav-link">
-									<ShoppingBagOutlinedIcon/>
-								My Cart
-								</a>
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link href="/about">
-								<a className="nav-link">About Page</a>
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link href="/contact">
-								<a className="nav-link">Contact Page</a>
-							</Link>
-						</li>
-					</ul>
+					<div className="mx-auto d-flex flex-column flex-md-row flex-fill align-items-center">
+						<Link href="/">
+							<a className="navbar-brand m0">Burger Corner</a>
+						</Link>
+						<ul className="navbar-nav mb-2 mb-lg-0 d-flex align-items-center justify-content-md-evenly flex-fill">
+							<li className="nav-item">
+								<Link href="/menu">
+									<a className="nav-link">Menu</a>
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link href="/shoppingCart">
+									<a className="nav-link">
+										<ShoppingBagOutlinedIcon />
+										My Cart
+									</a>
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link href="/about">
+									<a className="nav-link">About Page</a>
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link href="/contact">
+									<a className="nav-link">Contact Page</a>
+								</Link>
+							</li>
+						</ul>
 
-					<div className="form-check form-switch">
-						<input
-							checked={darkModeEnabled}
-							className="form-check-input"
-							onChange={() =>
-								setDarkModeEnabled(!darkModeEnabled)
-							}
-							type="checkbox"
-							role="switch"
-							id="flexSwitchCheckDefault"
-						/>
-						<label
-							className={`form-check-label ${
-								darkModeEnabled ? "text-light bg-dark" : ""
-							}`}
-							htmlFor="flexSwitchCheckDefault"
-						>
-							Dark Mode
-						</label>
+						<div className="form-check form-switch">
+							<input
+								checked={darkModeEnabled}
+								className="form-check-input"
+								onChange={() =>
+									setDarkModeEnabled(!darkModeEnabled)
+								}
+								type="checkbox"
+								role="switch"
+								id="flexSwitchCheckDefault"
+							/>
+							<label
+								className={`form-check-label ${
+									darkModeEnabled ? "text-light bg-dark" : ""
+								}`}
+								htmlFor="flexSwitchCheckDefault"
+							>
+								Dark Mode
+							</label>
+						</div>
 					</div>
 				</div>
 			</div>
