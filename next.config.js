fb7c49615b3126fb978116640d23473c892100a8
Jonathan Carter <carterj@belletire.com>
2022-10-12 15:34:27 -0400

    fixed confgi

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
-module.exports = {
-  images: {
-    domains: ['pexels.com'],
-  },
-}
-module.exports = nextConfig
+	reactStrictMode: true,
+	images: {
+		domains: ["images.pexels.com"],
+	},
+};
+
+module.exports = nextConfig;
