727488d23c6c0b0c76586e0fb1b6182f3c0c464e
Jason Johnson <jas2johns@gmail.com>
2022-11-08 17:05:15 -0800

    tinkered with CSS

--- data/menu.js
+++ data/menu.js
@@ -8,7 +8,6 @@ export const allDayMenu = {
 					description:
 						'The Go 2 classic Single Mustard Grilled Patty, "white" American Cheese, fresh shredded lettuce, tomato.',
 					image: "Classic.svg",
-					backgroundColor: "#F0FF00",
 				},
 				{
 					id: 2,
@@ -21,7 +20,6 @@ export const allDayMenu = {
 					description:
 						"The Hawaiian burger; BBQ Grilled burger 6oz, Chopped Grilled onions, grilled pineapple slice, Monterey jack crisp, and a choice of our house teriyaki Sauce or BBQ sauce.",
 					image: "Hawaiin.svg",
-					backgroundColor: "#FF00DB",
 				},
 				{
 					id: 4,
--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/contact.module.css";
+import styles from "../styles/Contact.module.css";
 
 export default function Home() {
 	return (
@@ -14,16 +14,18 @@ export default function Home() {
 			<main>
 				<div className={styles["img"]}>
 					<h1>Contact us at Burger Corner</h1>
-
-					<p>
-						We know you like our burgers, now what else can we for
-						you?
-					</p>
-					<p>Did you want a job with us? To send us fan mail?</p>
-					<p>
-						Or to cater that special event for you, well if its yes
-						to any of the above, you&apos;re in the right place 😁
-					</p>
+					
+					<div className={styles["container"]}>
+						<p>
+							We know you like our burgers, now what else can we for
+							you?
+						</p>
+						<p>Did you want a job with us? To send us fan mail?</p>
+						<p>
+							Or to cater that special event for you, well if its yes
+							to any of the above, you&apos;re in the right place 😁
+						</p>
+					</div>
 				</div>
 			</main>
 
