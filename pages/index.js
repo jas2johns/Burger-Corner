735f80f7ab774ef87fe74fb5e42013bd1a04ed35
Jason Johnson <jas2johns@gmail.com>
2022-11-14 15:32:29 -0800

    home pg content and btn link

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,30 @@ export default function Home() {
 				<main>
 					
 						<div className={styles["home"]}>
-							<div>
+							<div className={styles["homeContent"]}>
+							<div className="glass">
+									<div className={styles["txt"]}>
+									<h2>Welcome to Burger Corner!</h2>
+									<p>
+										Here at Burger Corner, we strive to make excellent burgers at 
+										a fair price.
+										<br/>
+										Please try anything from our menu, I promise you wont regret it!
 
-								
+									</p>							
+								</div>
 							</div>
+							<span>
+							<Link href="/menu">
+								<a>
+								<button type="button" className="btn btn-primary btn-lg">
+									<h3>Order</h3>
+								</button>
+								</a>
+							</Link>
+							</span>
+							</div>
+
 						</div>
 					
 				</main>
