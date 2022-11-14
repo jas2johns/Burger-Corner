3d9ef0d766eb1246523a7d0869403c9ec1b88d55
Jason Johnson <jas2johns@gmail.com>
2022-11-13 18:11:00 -0800

    back to normal

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/Contact.module.css";
+import styles from "../styles/contact.module.css";
 
 export default function Home() {
 	return (
